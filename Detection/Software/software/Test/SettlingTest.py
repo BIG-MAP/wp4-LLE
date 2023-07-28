d69279bfcdc8bd3037b023414baa374adea98cbd
Rodrigo <rodr@itu.dk>
2023-07-28 17:33:14 +0200

    Created settling test

---
+++ Detection/Software/software/Test/SettlingTest.py
@@ -0,0 +1,30 @@
+import time
+import requests
+
+
+def check_finished():
+    finished = False
+    time.sleep(1)
+
+    r = requests.get('http://127.0.0.1:8000/status')
+    result = r.json()
+    while result['status'] != 'finished':
+        print("Status: " + result['status'])
+        r = requests.get('http://127.0.0.1:8000/status')
+        result = r.json()
+        time.sleep(2)
+
+    print("Status: " + str(r.json()['status']))
+
+def settling_test(settlingTime:int,scanInterval:int):
+    print("Settling")
+    r = requests.post('http://127.0.0.1:8000/startSettling',
+                      json={'settlingSettings': {'scanInterval': scanInterval, 'settlingTime': settlingTime}})
+    check_finished()
+
+def main():
+    settling_test(settlingTime=900,scanInterval=200)
+
+
+if __name__ == "__main__":
+    main()
